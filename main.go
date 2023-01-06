f09b1dd415a7686768caa92462df44e56ed1101e
Afaan Bilal <afaanbilal@gmail.com>
2023-01-06 10:40:48 +0530

    ✨ Alarm labels

--- main.go
+++ main.go
@@ -25,19 +25,30 @@ type Time struct {
 	S int
 }
 
-func main() {
-	var alarms = [4]Time{{13, 20, 0}, {16, 15, 0}, {17, 40, 0}, {19, 20, 0}}
+type Alarm struct {
+	time  Time
+	label string
+}
 
-	var alarmHandles = [4]chan string{}
-	for i := 0; i < 4; i++ {
-		alarmHandles[i] = SetAlarm(alarms[i], func() {
-			fmt.Println("Alarm received", i)
+func main() {
+	var alarms = [5]Alarm{
+		{time: Time{6, 40, 0}, label: "Zuhr"},
+		{time: Time{13, 20, 0}, label: "Zuhr"},
+		{time: Time{16, 15, 0}, label: "Asr"},
+		{time: Time{17, 40, 0}, label: "Maghrib"},
+		{time: Time{19, 20, 0}, label: "Isha"},
+	}
+
+	var alarmHandles = [5]chan string{}
+	for i := 0; i < 5; i++ {
+		alarmHandles[i] = SetAlarm(alarms[i].time, func() {
+			fmt.Println("Alarm received: ", alarms[i].label)
 			PlayBell()
 		})
 	}
 
 	// Block.
-	for i := 0; i < 4; i++ {
+	for i := 0; i < 5; i++ {
 		<-alarmHandles[i]
 	}
 }
